806b2829ecbc272fdc24cb8d3a6ebf8b790977f1
suusanex <suusan@himado.net>
2024-12-09 20:13:12 +0900

    WPFの基本テンプレートに対して、最小限のGenericHost組み込みを行うコード

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WpfApp1
 {
@@ -9,6 +11,25 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        private IHost? _host;
+
+        private async void OnStartup(object sender, StartupEventArgs e)
+        {
+            _host = Host.CreateDefaultBuilder(e.Args)
+                .ConfigureServices(collection =>
+                {
+                    collection.AddHostedService<ApplicationHostService>();
+                    collection.AddTransient<MainWindow>();
+                })
+                .Build();
+
+            await _host.StartAsync();
+        }
+
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+
+        }
     }
 
 }
---
+++ WpfApp1/ApplicationHostService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfApp1
+{
+    public class ApplicationHostService(IServiceProvider _serviceProvider) : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _MainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            _MainWindow.Show();
+            await Task.CompletedTask;
+        }
+
+        private Window? _MainWindow;
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
